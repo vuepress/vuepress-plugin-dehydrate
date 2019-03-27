0a65dcfe7fd06b76f20ce4299a9af282a83a6b72
Shigma <1700011071@pku.edu.cn>
2019-03-27 18:27:07 +0800

    chore: CI and better snapshots

--- __tests__/docs/.vuepress/config.js
+++ __tests__/docs/.vuepress/config.js
@@ -1,8 +1,27 @@
+const { resolve } = require('path')
+const CSSExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
+  dest: resolve(__dirname, '../../dist'),
+
   plugins: [
     [require('../../..'), {
       noScript: 'noscript.html',
     }],
   ],
+
   evergreen: false,
+
+  chainWebpack (config) {
+    // do not use chunk hash in js
+    config
+      .output.filename('assets/js/[name].js')
+
+    // do not use chunk hash in css
+    config
+      .plugin('extract-css')
+      .use(CSSExtractPlugin, [{
+        filename: 'assets/css/styles.css',
+      }])
+  },
 }
